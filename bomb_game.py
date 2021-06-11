8ca45abe8a1c6d19d03d5c8c3f24f3e53d477c3f
SuminJN <71697350+SuminJN@users.noreply.github.com>
2021-06-11 20:27:05 +0900

    Add files via upload

--- bomb_game.py
+++ bomb_game.py
@@ -6,20 +6,23 @@ pygame.init()  # pygame initialization
 
 # global variable
 BLACK = (0,0,0)
-size = [600, 800] # Set the height and width of the screen
+size = [700, 800] # Set the height and width of the screen
 screen = pygame.display.set_mode(size)
-game_font = pygame.font.Font(None, 100)
+game_font = pygame.font.Font(None, 150)
+background_image = pygame.image.load('background.png')
 
 done = False
 clock = pygame.time.Clock()
 
+# runGame Function
 def runGame():
+
     # Set bombs
     bomb_image = pygame.image.load('bomb.png')
     bomb_image = pygame.transform.scale(bomb_image, (60, 60))
     bombs = []
 
-    for i in range(5):
+    for i in range(7):
         rect = pygame.Rect(bomb_image.get_rect())
         rect.left = random.randint(0, size[0])
         rect.top = -100
@@ -32,25 +35,24 @@ def runGame():
     character = pygame.Rect(character_image.get_rect())
 
     character.left = size[0] // 2 - character.width // 2
-    character.top = size[1] - character.height
+    character.top = size[1] - character.height - 180
     character_dx = 0
-    character_dy = 0
-
 
     global done
     while not done:
         clock.tick(30)
         screen.fill(BLACK)
-
+        screen.blit(background_image, (0, 0))
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
                 break
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    character_dx = -5
+                    character_dx = -7
                 elif event.key == pygame.K_RIGHT:
-                    character_dx = 5
+                    character_dx = 7
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     character_dx = 0
@@ -85,12 +87,11 @@ def runGame():
         pygame.display.update()
 
 
-
 runGame()
 
 # Game Over
-msg = game_font.render("Game Over", True, (255, 255, 0))
-msg_rect = msg.get_rect(center=(int(size[0] / 2), int(size[1] / 2)))
+msg = game_font.render("Game Over", True, (255, 100, 100))
+msg_rect = msg.get_rect(center=(int(size[0] / 2), int(size[1] / 3)))
 screen.blit(msg, msg_rect)
 pygame.display.update()
 pygame.time.delay(2000)
